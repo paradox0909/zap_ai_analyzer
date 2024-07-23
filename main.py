31745efd89bdd51a519858a13ff3ee10fdb4da74
paradox <dokingkns0909@naver.com>
2024-07-23 21:00:25 +0900

    docker-mac localhost connection succesfully

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import requests
+import time
+from pprint import pprint
+from zapv2 import ZAPv2
 
+apiKey = 'paradox0909'
+target = 'https://public-firing-range.appspot.com'
+zap = ZAPv2(apiKey=apiKey, proxies={'http': 'http://172.17.0.1:8090', 'https': 'https://172.17.0.1:8090'})
+
+print('Active Scanning target {}'.format(target))
+scanID = zap.ascan.scan(target)
+while int(zap.ascan.status(scanID)) < 100:
+    # Loop until the scanner has finished
+    print('Scan progress %: {}'.format(zap.ascan.status(scanID)))
+    time.sleep(5)
+print('Active Scan completed')
+print('Hosts: {}'.format(', '.join(zap.core.hosts)))
+print('Alerts: ')
+pprint(zap.core.alerts(baseurl=target))
 responce = requests.get("http://172.17.0.1:8090")
 print(responce)
 
