be786974e8f4a352ef9ca4dda25702046d88c880
lizparadox_ <85692552+paradox0909@users.noreply.github.com>
2024-08-04 22:20:33 +0900

    request.py 업데이트

--- request.py
+++ request.py
@@ -1,5 +1,5 @@
 import request
 
-URL = "http://google.com?user=comp&num=42"
+URL = ["http://172.17.0.1:8090", "http://172.17.0.1:9001"]
 response = requests.get(URL)
 print("status code :", response.status_code)
