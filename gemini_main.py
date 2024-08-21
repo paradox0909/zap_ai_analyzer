9ac5b079534499e89d128f42bd7ee6e346d38b41
paradox <dokingkns0909@naver.com>
2024-08-22 00:04:46 +0900

    Project ENd

--- gemini_main.py
+++ gemini_main.py
@@ -19,13 +19,13 @@ with open(login_websource_dir, 'r', encoding='utf-8') as login_file:
 with open(json_path, 'r', encoding='utf-8') as json_file:
     parsed_json = json.load(json_file)
 
-web_source = {search_content}, {login_content}
-
-print(web_source)
+web_source = {login_content}, {search_content}
 
 response = model.generate_content(f"""
-웹 취약점 점검을 한 결과를 json으로 받았고, 이를 토대로 웹 취약점 점검이 정탐인지 오탐인지 판별해보려고 해.{parsed_json} 이게 json 데이터고,
-이게 점검한 웹 소스코드야. {web_source} 확인을 해보고 웹 취약점 점검이 정탐인지 오탐인지 판별해주고, 출력할때는, [search.php : 정탐] [login.php : 오탐] 이렇게 출력해주고, 출력한 이유도 알려줘.
+웹 취약점 점검을 한 결과를 json으로 받았고, 이를 토대로 웹 취약점 점검이 정탐인지 오탐인지 판별해보려고 해.{parsed_json} 이게 json 데이터야.
+json 데이터를 너가 전체적으로 봤으면 좋겠어. 
+이게 점검한 웹 소스코드야. {web_source} 이 공격이 실제 영향력이 있는지 "코드기반"으로 확인해줘. 
+"코드의 어느 부분이 취약한지 알려줘". 그 이유도 포함해서. 패치 방법은 알려주지 않아도 돼.
 """)
-
+# web_source = {login_content}, {search_content}
 print(response.text)
--- main.py
+++ main.py
@@ -2,17 +2,23 @@ import time
 import json
 import os
 from zapv2 import ZAPv2
+import google.generativeai as genai
 
 target = 'http://172.17.0.3'
 apiKey = 'paradox0909'
 
+search_websource_dir = './websource/search.php'
+login_websource_dir = './websource/login.php'
+json_path = 'vuln_check.json'
+
 zap = ZAPv2(apikey=apiKey)
 
-def get_next_filename(prefix='test_json_', extension='.json'):
-    i = 1
-    while os.path.exists(f"{prefix}{i}{extension}"):
-        i += 1
-    return f"{prefix}{i}{extension}"
+def save_to_vuln_check_json(result, prefix='vuln_check', extension='.json'):
+    filename = f"{prefix}{extension}"
+    
+    with open(filename, 'w') as f:
+        json.dump(result, f, indent=4)
+    print(f'Result saved to {filename}')
 
 def run_spider():
     print(f'Spidering target {target}')
@@ -78,18 +84,36 @@ def run_active_scan():
         'Hosts': zap.core.hosts,
         'Alerts': high_risk_alerts
     }
+    save_to_vuln_check_json(result)
 
-    filename = get_next_filename()
-    with open(filename, 'w') as f:
-        json.dump(result, f, indent=4)
+def search():
+    GOOGLE_API_KEY = "AIzaSyB4mgp-9DoH8njhPp9B66S1wf48TtjNBr0"
+    genai.configure(api_key=GOOGLE_API_KEY)
+    model = genai.GenerativeModel('gemini-pro')
+    with open(search_websource_dir, 'r', encoding='utf-8') as search_file:
+        search_content = search_file.read()
+
+    with open(login_websource_dir, 'r', encoding='utf-8') as login_file:
+        login_content = login_file.read()
+
+    with open(json_path, 'r', encoding='utf-8') as json_file:
+        parsed_json = json.load(json_file)
     
-    print(f'Result saved to {filename}')
+    web_source = f"{login_content}\n{search_content}"
+    
+    response = model.generate_content(f"""
+웹 취약점 점검을 한 결과를 json으로 받았고, 이를 토대로 웹 취약점 점검이 정탐인지 오탐인지 판별해보려고 해.{parsed_json} 이게 json 데이터야.
+json 데이터를 너가 전체적으로 봤으면 좋겠어. 
+점검한 웹 소스코드야. {web_source} 이 점검 결과가 실제 영향력이 있는지 "코드기반"으로 확인해줘. 
+"이유를 포함해서 코드의 어느 부분이 취약한지 알려줘". 코드의 전체 부분도 출력 부탁. [취약점 명, 전체 코드,취약한 부분의 코드, 이유] 이런식으로 나눠서.
+""")
+    print(response.text)
 
 if __name__ == '__main__':
     run_spider()
-    time.sleep(1)
-    # ajax_spider()
+
     time.sleep(1)
     run_active_scan()
     time.sleep(1)
+    search()
     print("END CODE")
